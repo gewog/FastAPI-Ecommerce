82d70e5b56090a089d517985438502c053cd09aa
gewoggewog <gewoggewog@gmail.com>
2025-09-01 15:18:15 +0300

    SQLAlchemy models created

--- app/backend/db.py
+++ app/backend/db.py
@@ -4,19 +4,29 @@ from sqlalchemy import text
 from sqlalchemy.orm import DeclarativeBase
 
 from sqlalchemy.ext.asyncio import async_sessionmaker, create_async_engine
-from settings import setting  # Экземпляр класса Settings
+from app.backend.settings import setting  # Экземпляр класса Settings
 
+from app.models.category import Category, Base
+from app.models.products import Product, Base
 
 engine = create_async_engine(setting.get_path, echo=True)
 session = async_sessionmaker(bind=engine)
 
 
+async def create_tables():
+    try:
+        async with engine.begin() as conn:
+            await conn.run_sync(Base.metadata.create_all)
+            print("Таблицы успешно созданы!")
+    except Exception as e:
+        print(f"Ошибка при создании таблиц: {e}")
+
 async def get_version():
     """Тестовое получение версии постгерс"""
     async with session() as ss:
         res = await ss.execute(text("select version();"))
-        print(res.fetchall())
+        print(res.fetchone())
 
 
 if __name__ == "__main__":
-    asyncio.run(get_version())
+    asyncio.run(create_tables())
---
+++ app/models/products.py
@@ -0,0 +1,31 @@
+"""
+Модель SQLAlchemy продуктов
+"""
+from typing import Annotated
+
+from sqlalchemy import (Integer, ForeignKey, String, Boolean, Float)
+from sqlalchemy.orm import (Mapped, mapped_column, relationship, DeclarativeBase)
+
+from app.models.category import Base
+
+
+str_type = Annotated[str, mapped_column(String)] # Кастомный тип данных 1
+int_type = Annotated[int, mapped_column(Integer)] # Кастомный тип данных 2
+
+
+class Product(Base):
+    """SQLAlchemy модель продуктов"""
+    __tablename__ = "products"
+
+    id: Mapped[int] = mapped_column(Integer, primary_key=True, index=True)
+    name: Mapped[str] = mapped_column(String)
+    slug: Mapped[str] = mapped_column(String, unique=True, index=True)
+    description: Mapped[str_type]
+    price: Mapped[int_type]
+    image_url: Mapped[str_type]
+    stock: Mapped[int_type]
+    rating: Mapped[float] = mapped_column(Float)
+    is_active: Mapped[bool] = mapped_column(Boolean, default=True)
+    category_id: Mapped[int] = mapped_column(ForeignKey("categories.id", ondelete="CASCADE"))
+    category: Mapped["Category"] = relationship(
+        back_populates="product") # Отношение многие к 1 с таблицей Category
